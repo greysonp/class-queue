0981d26d59d46c45529a47ec265bbd3d3cb06a97
Greyson Parrelli <greyson.parrelli@gmail.com>
2016-03-06 21:42:25 -0800

    Revert "Cleaned out master branch."
    This reverts commit bd3947378d412776db1c868193cb94374d4eae87.

--- README.md
+++
@@ -1,3 +0,0 @@
-# Class Queue
-
-All work is done on the gh-pages branch. View the project [here](https://greysonp.github.io/class-queue).
---
+++ teacher/index.js
@@ -0,0 +1,61 @@
+(function() {
+
+    init();
+
+    function init() {
+        getFirebase().child('teacher_password').on('value', function(snapshot) {
+            renderLoginPage(snapshot.val());
+        });
+    }
+
+    function renderLoginPage(sessionKey) {
+        render('/teacher/page/login.html', function() {
+            $('#js-form').submit(function(e) {
+                var userInput = $('#js-teacher-password').val();
+                if (sessionKey === userInput) {
+                    e.preventDefault();
+                    renderDashboard();
+                } else {
+                    alert('try again');
+                }
+            });
+        });
+    }
+
+    function renderDashboard() {
+        render('/teacher/page/dashboard.html', function() {
+
+            // Submit event
+            $('#js-form').submit(function(e) {
+                e.preventDefault();
+
+                var sessionKey = $('#js-session-key').val();
+                getFirebase().child('session_key').set(sessionKey);
+            });
+
+            // Render question list
+            var source = $("#js-questions-template").html();
+            var template = Handlebars.compile(source);
+            getFirebase().child('questions').on('value', function(snapshot) {
+                var questions = snapshot.val();
+                console.log(questions);
+                var html = template({
+                    questions: questions
+                });
+                $('#js-questions').html(html);
+            });
+        });
+    }
+
+    function render(page, callback) {
+        $.get(page, function(data) {
+            $('#js-container').html(data);
+            callback();
+        });
+    }
+
+    function getFirebase() {
+        return new Firebase('https://class-queue.firebaseio.com/');
+    }
+
+})();
